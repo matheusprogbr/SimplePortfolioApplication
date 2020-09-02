2fb78350259cb75dab0df76fecfc643ba9d935fa
Matheus Henrique <matheusprogrj@gmail.com>
2020-09-02 17:57:31 -0300

    Added nunjucks application

---
+++ SimplePortfolioApplication/about_data.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  avatar_url: '/IMG-20180416-WA0001.jpg',
+  name: 'Matheus Henrique da Silva dos Santos',
+  role: 'Estudante de Engenharia da Informação',
+  place: 'UFABC',
+  place_url:'https://ufabc.edu.br',
+  content: 'Desenvolvedor Web Fullstack e Entusiasta na área de Segurança da Informação',
+  links: [
+    {name:'Github', url:'https://www.github.com/matheusprogrj'},
+    {name:'Twitter' ,url:'https://www.twitter.com'},
+    {name:'Linkedin' ,url:'https://www.linkedin.com'},
+    {name:'BitBucket' ,url:'https://www.bitbucket.com'},
+  ]
+  
+};
---
+++ SimplePortfolioApplication/data.js
@@ -0,0 +1,39 @@
+module.exports = [
+  {
+    id:'GykTLqODQuU',
+    title:'Formulário Animado com JS puro e CSS Animation',
+    duration:'57 min',
+    price: 'Free'
+  },
+  {
+    id:'GykTLqODQuU',
+    title:'Formulário Animado com JS puro e CSS Animation',
+    duration:'57 min',
+    price: 'Free'
+  },
+  {
+    id:'GykTLqODQuU',
+    title:'Formulário Animado com JS puro e CSS Animation',
+    duration:'57 min',
+    price: 'Free'
+  },
+  {
+    id:'GykTLqODQuU',
+    title:'Formulário Animado com JS puro e CSS Animation',
+    duration:'57 min',
+    price: 'Free'
+  },
+  {
+    id:'GykTLqODQuU',
+    title:'Formulário Animado com JS puro e CSS Animation',
+    duration:'57 min',
+    price: 'Free'
+  },
+  {
+    id:'EhnXaybirdA',
+    title:'Como criar seu Portfólio - RocketSeat',
+    duration:'43 min',
+    price: 'Free'
+  },
+
+];
